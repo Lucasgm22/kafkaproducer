7ef9378dde904fc08fe1db71bffb5f2f4e749699
lucas_lsgsma <lucas.guimaraes@qintess.com>
2022-11-30 14:53:49 -0300

    first commit

---
+++ src/main/java/com/poc/kafkproducer/KafkproducerApplication.java
@@ -0,0 +1,13 @@
+package com.poc.kafkproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkproducerApplication.class, args);
+	}
+
+}
