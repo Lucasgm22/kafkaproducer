484f6bb74e036d4b0de679c91462c8003b005058
lucas_lsgsma <lucas.guimaraes@qintess.com>
2022-12-01 09:55:57 -0300

    unused import

--- src/main/java/com/poc/kafkproducer/service/Consumer.java
+++ src/main/java/com/poc/kafkproducer/service/Consumer.java
@@ -3,10 +3,7 @@ package com.poc.kafkproducer.service;
 import com.poc.kafkproducer.data.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
-
-import java.util.Map;
 
 @Service
 @Slf4j
