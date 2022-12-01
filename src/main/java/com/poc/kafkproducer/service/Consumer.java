6bab9f4d67cfee3684ba0d7122145db3ee6bbf4a
lucas_lsgsma <lucas.guimaraes@qintess.com>
2022-12-01 10:10:05 -0300

    consuming key

--- src/main/java/com/poc/kafkproducer/service/Consumer.java
+++ src/main/java/com/poc/kafkproducer/service/Consumer.java
@@ -3,6 +3,9 @@ package com.poc.kafkproducer.service;
 import com.poc.kafkproducer.data.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +13,7 @@ import org.springframework.stereotype.Service;
 public class Consumer {
 
     @KafkaListener(topics = "quickstart-events", groupId = "poc")
-    public void consume(Message.Value message) {
-        log.info("Message received: {}", message);
+    public void consume(@Header(KafkaHeaders.RECEIVED_KEY) Message.Key key, @Payload Message.Value message) {
+        log.info("Message key: {}, content: {}", key, message);
     }
 }
