d75e00c0b91f387bbc744bf1a4d6ca29f1195cc6
lucas_lsgsma <lucas.guimaraes@qintess.com>
2022-12-01 10:57:13 -0300

    refactor App name

--- src/main/java/com/poc/springkafkapoc/SpringKafkaPocApplication.java
+++ src/main/java/com/poc/springkafkapoc/SpringKafkaPocApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkproducerApplication {
+public class SpringKafkaPocApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KafkproducerApplication.class, args);
+		SpringApplication.run(SpringKafkaPocApplication.class, args);
 	}
 
 }
--- src/test/java/com/poc/springkafkapoc/SpringKafkaPocApplicationTests.java
+++ src/test/java/com/poc/springkafkapoc/SpringKafkaPocApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KafkproducerApplicationTests {
+class SpringKafkaPocApplicationTests {
 
 	@Test
 	void contextLoads() {
